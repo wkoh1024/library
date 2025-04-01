bef2d006a1e02a9b3cc7eb23873a7ec4ded6dd6f
Wayne <34191804+wkoh1024@users.noreply.github.com>
2025-04-01 15:44:21 -0400

    Added test books

--- script.js
+++ script.js
@@ -17,10 +17,18 @@ function Book(title, author, pages, isRead) {
   }
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(Book) {
   // take params, create a book then store it in the array
+  if (Book.protoype !== Object.getPrototypeOf(this.Book)) {
+    throw Error(`Object is not a "Book" item!`);
+  }
+  myLibrary.push(Book);
 }
 
-// let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-// console.log(theHobbit.info());
+let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+let prideAndPrejudice = new Book("Pride and Prejudice", "Jane Austen", 432, true);
+let nineteenEightyFour = new Book("1984", "George Orwell", 328, false);
+let toKillAMockingbird = new Book("To Kill a Mockingbird", "Harper Lee", 281, true);
+let theCatcherInTheRye = new Book("The Catcher in the Rye", "J.D. Salinger", 224, false);
+let theGreatGatsby = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
 
