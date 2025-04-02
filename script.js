5a09fd55d5b812404f53432c99d067d00b79e1c3
Wayne <waynekoh1024@gmail.com>
2025-04-02 17:21:50 -0400

    Added object validation and dummy data

--- script.js
+++ script.js
@@ -13,22 +13,31 @@ function Book(title, author, pages, isRead) {
 
   this.info = function() {
     let isReadString = isRead ? "read" : "not read yet";
-    return `ID${this.id} "${this.title}" by ${this.author}, ${this.pages} pages, ${isReadString}`;
+    return `${this.id}, "${this.title}" by ${this.author}, ${this.pages} pages, ${isReadString}`;
   }
 }
 
-function addBookToLibrary(Book) {
+function addBookToLibrary(bookToAdd) {
   // take params, create a book then store it in the array
-  if (Book.protoype !== Object.getPrototypeOf(this.Book)) {
+  if (!(bookToAdd instanceof Book)) {
     throw Error(`Object is not a "Book" item!`);
   }
-  myLibrary.push(Book);
+  myLibrary.push(bookToAdd);
 }
 
-let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-let prideAndPrejudice = new Book("Pride and Prejudice", "Jane Austen", 432, true);
-let nineteenEightyFour = new Book("1984", "George Orwell", 328, false);
-let toKillAMockingbird = new Book("To Kill a Mockingbird", "Harper Lee", 281, true);
-let theCatcherInTheRye = new Book("The Catcher in the Rye", "J.D. Salinger", 224, false);
-let theGreatGatsby = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const prideAndPrejudice = new Book("Pride and Prejudice", "Jane Austen", 432, true);
+const nineteenEightyFour = new Book("1984", "George Orwell", 328, false);
+const toKillAMockingbird = new Book("To Kill a Mockingbird", "Harper Lee", 281, true);
+const theCatcherInTheRye = new Book("The Catcher in the Rye", "J.D. Salinger", 224, false);
+const theGreatGatsby = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
 
+addBookToLibrary(theHobbit);
+addBookToLibrary(prideAndPrejudice);
+addBookToLibrary(nineteenEightyFour);
+addBookToLibrary(toKillAMockingbird);
+addBookToLibrary(theCatcherInTheRye);
+addBookToLibrary(theGreatGatsby);
+
+
+console.log(myLibrary);
