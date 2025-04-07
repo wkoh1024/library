e8bb8dc8421b357aa1043b394efe4fd26571bd7f
Wayne <waynekoh1024@gmail.com>
2025-04-07 17:46:40 -0400

    Displayed books and automatically add books to library upon creation

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const myLibrary = [];
 
+function addBookToLibrary(bookToAdd) {
+  // take params, create a book then store it in the array
+  if (!(bookToAdd instanceof Book)) {
+    throw Error(`Object is not a "Book" item!`);
+  }
+  myLibrary.push(bookToAdd);
+}
+
 function Book(title, author, pages, isRead) {
   // the constructor...
   if (!new.target) {
@@ -15,14 +23,15 @@ function Book(title, author, pages, isRead) {
     let isReadString = isRead ? "read" : "not read yet";
     return `${this.id}, "${this.title}" by ${this.author}, ${this.pages} pages, ${isReadString}`;
   }
+
+  addBookToLibrary(this);
 }
 
-function addBookToLibrary(bookToAdd) {
-  // take params, create a book then store it in the array
-  if (!(bookToAdd instanceof Book)) {
-    throw Error(`Object is not a "Book" item!`);
+function removeBookFromLibrary(bookToRemove) {
+  const index = myLibrary.indexOf(bookToRemove);
+  if (index > -1) {
+    myLibrary.splice(index, 1);
   }
-  myLibrary.push(bookToAdd);
 }
 
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
@@ -32,12 +41,21 @@ const toKillAMockingbird = new Book("To Kill a Mockingbird", "Harper Lee", 281, 
 const theCatcherInTheRye = new Book("The Catcher in the Rye", "J.D. Salinger", 224, false);
 const theGreatGatsby = new Book("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
 
-addBookToLibrary(theHobbit);
-addBookToLibrary(prideAndPrejudice);
-addBookToLibrary(nineteenEightyFour);
-addBookToLibrary(toKillAMockingbird);
-addBookToLibrary(theCatcherInTheRye);
-addBookToLibrary(theGreatGatsby);
-
 
 console.log(myLibrary);
+
+const bookshelf = document.querySelector("#bookshelf");
+console.log(bookshelf);
+
+for (let i = 0; i < myLibrary.length; i++) {
+  let newItem = document.createElement("div");
+  newItem.classList.add("book");
+  newItem.setAttribute("id", myLibrary[i].id);
+  newItem.innerHTML = `
+    <h2>${myLibrary[i].title}</h2>
+    <p>Author: ${myLibrary[i].author}</p>
+    <p>Pages: ${myLibrary[i].pages}</p>
+    <p>Status: ${myLibrary[i].isRead ? "Read" : "Not Read Yet"}</p>
+  `;
+  bookshelf.appendChild(newItem);
+}
